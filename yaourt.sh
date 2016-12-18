b98e4a85746059672795f70d96f173f92e80e3d3
logickee <logickee@gmail.com>
2016-12-18 15:05:10 +0800

    init commit

---
+++ yaourt.sh
@@ -0,0 +1,11 @@
+yaourt -S --noconfirm pacaur
+
+yaourt -S --noconfirm visual-paradigm-community
+yaourt -S --noconfirm nixnote
+
+
+
+
+pacaur -S --noconfirm grive-tools
+
+pacaur -S --noconfirm nixnote spotify telegram-desktop-bin virtualbox-ext-oracle    gtk-theme-arc-git
